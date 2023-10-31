7dbb3b2d22cffb8b6a96ae9505beebc145ff46d8
de-brasi <leskin.ilia.public@yandex.ru>
2023-10-31 17:45:44 +0300

    better

--- src/main/java/edu/hw3/Task6.java
+++ src/main/java/edu/hw3/Task6.java
@@ -4,11 +4,11 @@ import java.util.PriorityQueue;
 
 interface StockMarket {
     /** Добавить акцию */
-    public void add(Stock stock);
+    void add(Stock stock);
     /** Удалить акцию */
-    public void remove(Stock stock);
+    void remove(Stock stock);
     /** Самая дорогая акция */
-    public Stock mostValuableStock();
+    Stock mostValuableStock();
 }
 
 public class Task6 {
@@ -23,13 +23,12 @@ public class Task6 {
 
         @Override
         public void remove(Stock stock) {
-            final var toRemoveObj = stockStorage.removeIf(
+            stockStorage.removeIf(
                 (suspect) -> (
-                    (suspect.getPrice() == stock.getPrice())
-                        && (suspect.getCompanyName() == stock.getCompanyName())
+                    (suspect.getPrice().equals(stock.getPrice()))
+                        && (suspect.getCompanyName().equals(stock.getCompanyName()))
                 )
             );
-            stockStorage.remove(stock);
         }
 
         @Override
@@ -37,7 +36,7 @@ public class Task6 {
             return stockStorage.peek();
         }
 
-        private final PriorityQueue<Stock> stockStorage = new PriorityQueue<Stock>(
+        private final PriorityQueue<Stock> stockStorage = new PriorityQueue<>(
             (lhs, rhs) -> {
                 return -1 * lhs.getPrice().compareTo(rhs.getPrice());
             }
