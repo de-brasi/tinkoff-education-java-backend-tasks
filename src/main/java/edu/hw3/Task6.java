2053553c92d07db6aa8be11177e4fb8d22fa3778
de-brasi <leskin.ilia.public@yandex.ru>
2023-10-31 17:47:39 +0300

    linter fix

--- src/main/java/edu/hw3/Task6.java
+++ src/main/java/edu/hw3/Task6.java
@@ -2,19 +2,11 @@ package edu.hw3;
 
 import java.util.PriorityQueue;
 
-interface StockMarket {
-    /** Добавить акцию */
-    void add(Stock stock);
-    /** Удалить акцию */
-    void remove(Stock stock);
-    /** Самая дорогая акция */
-    Stock mostValuableStock();
-}
-
 public class Task6 {
     private Task6() {
         // not allowed
     }
+
     public static class Market implements StockMarket {
         @Override
         public void add(Stock stock) {
