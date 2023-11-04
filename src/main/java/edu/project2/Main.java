eb6729a6360ac9d764477059a4f057007c094dbd
de-brasi <leskin.ilia.public@yandex.ru>
2023-11-04 21:48:21 +0300

    prepared for random tests (just printing random maze)

--- src/main/java/edu/project2/Main.java
+++ src/main/java/edu/project2/Main.java
@@ -4,13 +4,14 @@ import edu.project2.generators.RandomizedDFSGenerator;
 import edu.project2.prettyprinter.PrettyPrinter;
 import edu.project2.util.Coordinate;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         final var generator = new RandomizedDFSGenerator();
-        final var maze = generator.generate(5, 5);
+        final var printer = new PrettyPrinter();
+
+        final var maze = generator.generate(20, 5);
         final var emptyCoordinates = new ArrayList<Coordinate>();
-        PrettyPrinter.Print(maze, emptyCoordinates);
+        printer.Print(maze, emptyCoordinates);
     }
 }
