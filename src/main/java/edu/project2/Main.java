3a898d240b3c60dc644a709bd8b1c58efd4f34a1
de-brasi <leskin.ilia.public@yandex.ru>
2023-11-05 22:32:34 +0300

    solvers comparing

--- src/main/java/edu/project2/Main.java
+++ src/main/java/edu/project2/Main.java
@@ -2,16 +2,37 @@ package edu.project2;
 
 import edu.project2.generators.RandomizedDFSGenerator;
 import edu.project2.prettyprinter.PrettyPrinter;
+import edu.project2.solvers.SolverBFS;
+import edu.project2.solvers.SolverDFS;
+import edu.project2.solvers.SolverRightHandRule;
 import edu.project2.util.Coordinate;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        final var generator = new RandomizedDFSGenerator();
+        final var randomGenerator = new RandomizedDFSGenerator();
+        final var dfsSolver = new SolverDFS();
+        final var bfsSolver = new SolverBFS();
+        final var rhrSolver = new SolverRightHandRule();
         final var printer = new PrettyPrinter();
 
-        final var maze = generator.generate(20, 5);
-        final var emptyCoordinates = new ArrayList<Coordinate>();
-        printer.Print(maze, emptyCoordinates);
+        final var maze = randomGenerator.generate(20, 20);
+
+        final var start = new Coordinate(0, 0);
+        final var end = new Coordinate(maze.getHeight() - 1, maze.getWidth() - 1);
+
+        var solution = dfsSolver.solve(maze, start, end);
+        printer.Print(maze, solution);
+
+        maze.resetCells();
+        System.out.println();
+
+        solution = bfsSolver.solve(maze, start, end);
+        printer.Print(maze, solution);
+
+        maze.resetCells();
+        System.out.println();
+
+        solution = rhrSolver.solve(maze, start, end);
+        printer.Print(maze, solution);
     }
 }
