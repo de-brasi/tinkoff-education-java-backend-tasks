51fddf4a284636a117266b86c784d9a2e9771ced
de-brasi <leskin.ilia.public@yandex.ru>
2023-11-05 14:56:34 +0300

    seems like done. need to test it

--- src/main/java/edu/project2/solvers/SolverDFS.java
+++ src/main/java/edu/project2/solvers/SolverDFS.java
@@ -1,8 +1,11 @@
 package edu.project2.solvers;
 
+import edu.project2.util.Cell;
 import edu.project2.util.Coordinate;
+import edu.project2.util.Direction;
 import edu.project2.util.Maze;
 import edu.project2.util.Solver;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class SolverDFS implements Solver {
@@ -10,6 +13,45 @@ public class SolverDFS implements Solver {
 
     @Override
     public ArrayList<Coordinate> solve(Maze maze, Coordinate start, Coordinate end) {
-        return new ArrayList<Coordinate>();
+        var solution = new ArrayDeque<Coordinate>();
+
+        maze.getCell(start).visit();
+        solution.addLast(start);
+
+        Coordinate curCoordinate;
+        Cell curCell;
+
+        Coordinate nextStepCoordinate;
+
+
+        while (!solution.isEmpty() && !solution.getLast().equals(end)) {
+            curCoordinate = solution.pollLast();
+            curCell = maze.getCell(curCoordinate);
+
+            for (var direction : this.directions) {
+                nextStepCoordinate = curCoordinate.coordinateFromDirection(direction);
+
+                if (curCell.checkWall(direction) || maze.getCell(nextStepCoordinate).checkIsVisited()) {
+                    continue;
+                }
+
+                solution.addLast(curCoordinate);
+                if (coordinateIsValid(nextStepCoordinate, maze) && !maze.getCell(nextStepCoordinate).checkIsVisited()) {
+                    maze.getCell(nextStepCoordinate).visit();
+                    solution.addLast(nextStepCoordinate);
+                }
+            }
+        }
+
+        return new ArrayList<>(solution);
+    }
+
+    private final Direction[] directions = new Direction[] {
+        Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT
+    };
+
+    private static boolean coordinateIsValid(Coordinate toCheck, Maze toCheckIn) {
+        return (0 <= toCheck.row() && toCheck.row() < toCheckIn.getHeight())
+            && (0 <= toCheck.col() && toCheck.col() < toCheckIn.getWidth());
     }
 }
