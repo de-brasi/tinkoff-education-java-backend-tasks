1d666cdd581a05972428f70324f74285f2e61eb1
de-brasi <leskin.ilia.public@yandex.ru>
2023-11-19 22:22:25 +0300

    better

--- src/test/java/edu/hw6/Task5Test.java
+++ src/test/java/edu/hw6/Task5Test.java
@@ -1,7 +1,5 @@
 package edu.hw6;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
