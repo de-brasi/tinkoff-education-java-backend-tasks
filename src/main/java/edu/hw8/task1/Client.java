e9e8b291b984f91ee75214fc5b3d74e812be26a2
de-brasi <leskin.ilia.public@yandex.ru>
2023-11-27 00:28:02 +0300

    task 1; current state: single thread server, connection with timeout in clients. row sockets not(!) used yet. correct connection behaviour, sending request and got correct response.

--- src/main/java/edu/hw8/task1/Client.java
+++ src/main/java/edu/hw8/task1/Client.java
@@ -8,13 +8,15 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.concurrent.TimeoutException;
 
 public class Client {
     public Client() {}
 
     public void Run() {
         try {
-            Socket client = new Socket(InetAddress.getByName("localhost"), 18080);
+            Socket client = tryMakeClientSocketInLoop(CONNECTION_TIMEOUT_MILLISECONDS);
+            LOGGER.info("Client: port created");
 
             var writer = new PrintWriter(client.getOutputStream(), true);
             var reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -26,27 +28,24 @@ public class Client {
 
             client.close();
         } catch (IOException e) {
-            LOGGER.info(e.getMessage());
+            LOGGER.info("Error: " + e.getMessage());
+        } catch (TimeoutException timeoutException) {
+            LOGGER.info(timeoutException.getMessage());
         }
     }
 
-    public static void main(String[] args) {
-        try {
-            Socket client = new Socket(InetAddress.getByName("localhost"), 18080);
-
-            var writer = new PrintWriter(client.getOutputStream(), true);
-            var reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
-            writer.println("абоба");
-            var res = reader.readLine();
-
-            LOGGER.info("Answer is: " + res);
-
-            client.close();
-        } catch (IOException e) {
-            LOGGER.info("Error: " + e.getMessage());
+    private Socket tryMakeClientSocketInLoop(int timeoutMillis) throws TimeoutException {
+        var startTime = System.nanoTime();
+        while (System.nanoTime() - startTime <= timeoutMillis * 1_000_000L) {
+            try {
+                return new Socket(InetAddress.getByName(HOST), PORT);
+            } catch (IOException ignored) {}
         }
+        throw new TimeoutException("Can't connect to server with host=%s; port=%s".formatted(HOST, PORT));
     }
 
     private final static Logger LOGGER = LogManager.getLogger();
+    private final String HOST = "localhost";
+    private final int PORT = 18080;
+    private final static int CONNECTION_TIMEOUT_MILLISECONDS = 100;
 }
--- src/main/java/edu/hw8/task1/Server.java
+++ src/main/java/edu/hw8/task1/Server.java
@@ -1,6 +1,5 @@
 package edu.hw8.task1;
 
-import edu.hw8.task1.NonSynchronizedCitationBase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.io.BufferedReader;
@@ -11,17 +10,17 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public Server() {
-
-    }
+    public Server() {}
 
     public void Run() {
-        try (ServerSocket server = new ServerSocket(18080)) {
+        try (ServerSocket server = new ServerSocket(PORT)) {
 
             Socket socket;
 
             while (true) {
+                LOGGER.info("Server: wait for client");
                 socket = server.accept();
+                LOGGER.info("Server: client got");
 
                 try(
                     var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -31,13 +30,22 @@ public class Server {
                     writer.println(citationBase.getCitation(response));
                 }
 
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    LOGGER.info(e.getMessage());
+                }
+
                 socket.close();
+                LOGGER.info("\n");
+                LOGGER.info("Server: client handled");
             }
         } catch (IOException ioException) {
             LOGGER.info("Error: " + ioException.getMessage());
         }
     }
 
-    private final static NonSynchronizedCitationBase citationBase = new NonSynchronizedCitationBase();
+    private final NonSynchronizedCitationBase citationBase = new NonSynchronizedCitationBase();
+    private final int PORT = 18080;
     private final static Logger LOGGER = LogManager.getLogger();
 }
