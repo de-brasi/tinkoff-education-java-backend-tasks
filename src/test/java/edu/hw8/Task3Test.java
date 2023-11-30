57d383b1e1c79ed830962f04c173ec5b7df1a1a6
de-brasi <leskin.ilia.public@yandex.ru>
2023-11-30 12:24:48 +0300

    multi-thread version test

--- src/test/java/edu/hw8/Task3Test.java
+++ src/test/java/edu/hw8/Task3Test.java
@@ -1,5 +1,6 @@
 package edu.hw8;
 
+import edu.hw8.task3.MultiThreadPasswordCracker;
 import edu.hw8.task3.SingleThreadPasswordCracker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task3Test {
     @Test
-    @DisplayName("Single thread passwords")
+    @DisplayName("Single-thread password cracker")
     public void test1() {
         var source = List.of(
             "Vasya 0cc175b9c0f1b6a831c399e269772661",
@@ -23,6 +24,27 @@ public class Task3Test {
         );
 
         var cracker = new SingleThreadPasswordCracker();
+        cracker.loadRecords(source);
+        final var res = cracker.getCrackedRecords();
+
+        assertThat(expected).isEqualTo(res);
+    }
+
+    @Test
+    @DisplayName("Multi-thread password cracker")
+    public void test2() {
+        var source = List.of(
+            "Vasya 0cc175b9c0f1b6a831c399e269772661",
+            "Masha 92eb5ffee6ae2fec3ad71c777531578f",
+            "Petya 4a8a08f09d37b73795649038408b5f33"
+        );
+        final var expected = Map.of(
+            "Vasya", "a",
+            "Masha", "b",
+            "Petya", "c"
+        );
+
+        var cracker = new MultiThreadPasswordCracker(3);
         cracker.loadRecords(source);
         final var res = cracker.getCrackedRecords();
 
