0d81687cd6cc72fb53f959aea9bd29dc68f768e0
de-brasi <leskin.ilia.public@yandex.ru>
2023-11-30 12:40:51 +0300

    multi-thread timings test

--- src/test/java/edu/hw8/Task3Test.java
+++ src/test/java/edu/hw8/Task3Test.java
@@ -2,6 +2,8 @@ package edu.hw8;
 
 import edu.hw8.task3.MultiThreadPasswordCracker;
 import edu.hw8.task3.SingleThreadPasswordCracker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
@@ -50,4 +52,41 @@ public class Task3Test {
 
         assertThat(expected).isEqualTo(res);
     }
+
+    @Test
+    @DisplayName("Multi-thread password cracker; efficiency of using additional thread")
+    public void test3() {
+        var source = List.of(
+            "Vasya 900150983cd24fb0d6963f7d28e17f72",
+            "Masha 202cb962ac59075b964b07152d234b70",
+            "Petya 30f5eef91a397e4bbab4d93aeb286aef"
+        );
+        final var expected = Map.of(
+            "Vasya", "abc",
+            "Masha", "123",
+            "Petya", "a1b"
+        );
+
+        MultiThreadPasswordCracker cracker;
+        Map<String, String> res;
+        long startTime;
+        long duration;
+        int threadsCount;
+
+        for (int i = 1; i < 9; i++) {
+            threadsCount = i;
+            cracker = new MultiThreadPasswordCracker(threadsCount);
+            cracker.loadRecords(source);
+            {
+                startTime = System.nanoTime();
+                res = cracker.getCrackedRecords();
+                duration = System.nanoTime() - startTime;
+                LOGGER.info("Threads count = %s; Duration = %s".formatted(threadsCount, duration));
+            }
+
+            assertThat(expected).isEqualTo(res);
+        }
+    }
+
+    private final static Logger LOGGER = LogManager.getLogger();
 }
