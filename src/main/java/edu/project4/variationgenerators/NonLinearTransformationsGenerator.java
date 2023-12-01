98fd797c614a5cb97b25aed822fd3c08ab59ed7a
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-01 21:10:40 +0300

    init

---
+++ src/main/java/edu/project4/variationgenerators/NonLinearTransformationsGenerator.java
@@ -0,0 +1,45 @@
+package edu.project4.variationgenerators;
+
+public class NonLinearTransformationsGenerator {
+    private NonLinearTransformationsGenerator() {}
+
+    public static Transformation getSinusoidalTransformation() {
+        return getSinusoidalTransformation(1, 1);
+    }
+
+    public static Transformation getSinusoidalTransformation(double xScale, double yScale) {
+        // TODO:
+    }
+
+    public static Transformation getSphericalTransformation() {
+        return getSphericalTransformation(1, 1);
+    }
+
+    public static Transformation getSphericalTransformation(double xScale, double yScale) {
+        // TODO:
+    }
+
+    public static Transformation getPolarTransformation() {
+        return getPolarTransformation(1, 1);
+    }
+
+    public static Transformation getPolarTransformation(double xScale, double yScale) {
+        // TODO:
+    }
+
+    public static Transformation getHeartTransformation() {
+        return getHeartTransformation(1, 1);
+    }
+
+    public static Transformation getHeartTransformation(double xScale, double yScale) {
+        // TODO:
+    }
+
+    public static Transformation getDiskTransformation() {
+        return getDiskTransformation(1, 1);
+    }
+
+    public static Transformation getDiskTransformation(double xScale, double yScale) {
+        // TODO:
+    }
+}
