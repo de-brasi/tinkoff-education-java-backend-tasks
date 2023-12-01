acf80c2861916977867e6bd76f830b1b4f5fd493
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-01 17:45:17 +0300

    delete Rect argument

--- src/main/java/edu/project4/renders/Renderer.java
+++ src/main/java/edu/project4/renders/Renderer.java
@@ -1,7 +1,6 @@
 package edu.project4.renders;
 
 import edu.project4.FractalImage;
-import edu.project4.utils.Rect;
 import edu.project4.Transformation;
 import java.util.List;
 
@@ -9,7 +8,7 @@ import java.util.List;
 public interface Renderer {
     FractalImage render(
         FractalImage canvas,
-        Rect world, List<Transformation> variations,
+        List<Transformation> variations,
         int samples, short iterPerSample, long seed
     );
 }
