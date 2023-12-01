37e5e630f09f52d62a493561fb5b61f02c0bce78
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-01 17:46:23 +0300

    init

---
+++ src/main/java/edu/project4/Transformation.java
@@ -0,0 +1,7 @@
+package edu.project4;
+
+import edu.project4.utils.Point;
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface Transformation extends Function<Point, Point> {}
