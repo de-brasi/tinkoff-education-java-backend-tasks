3372aaf171279efb9f8a23f4f89eb9a390ef6157
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-01 11:53:42 +0300

    init

---
+++ src/main/java/edu/project4/utils/Color.java
@@ -0,0 +1,55 @@
+package edu.project4.utils;
+
+public class Color {
+    public Color() {
+        red = 0;
+        green = 0;
+        blue = 0;
+    }
+
+    public Color(int r, int g, int b) {
+        red = r;
+        green = g;
+        blue = b;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public int r() {
+        return getRed();
+    }
+
+    public int g() {
+        return getGreen();
+    }
+
+    public int b() {
+        return getBlue();
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+
+    private int red;
+    private int green;
+    private int blue;
+}
