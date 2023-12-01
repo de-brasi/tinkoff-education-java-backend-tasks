240398211b9de820a413e7e09c8b170683300b55
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-01 11:33:27 +0300

    add method for enum representation as string

--- src/main/java/edu/project4/utils/ImageFormat.java
+++ src/main/java/edu/project4/utils/ImageFormat.java
@@ -1,3 +1,17 @@
 package edu.project4.utils;
 
-public enum ImageFormat {JPEG, BMP, PNG}
+public enum ImageFormat {
+    JPEG("jpeg"),
+    BMP("bmp"),
+    PNG("png");
+
+    ImageFormat(String formatStr) {
+        format = formatStr;
+    }
+
+    public String getStringRepresentation() {
+        return format;
+    }
+
+    private final String format;
+}
