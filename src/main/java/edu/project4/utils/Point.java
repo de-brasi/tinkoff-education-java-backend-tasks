56dda90361ab31934cba440a96f026e5e3f0696b
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-01 17:48:12 +0300

    add static creation-method

--- src/main/java/edu/project4/utils/Point.java
+++ src/main/java/edu/project4/utils/Point.java
@@ -1,3 +1,7 @@
 package edu.project4.utils;
 
-public record Point(double x, double y) {}
+public record Point(double x, double y) {
+    public static Point of(double x, double y) {
+        return new Point(x, y);
+    }
+}
