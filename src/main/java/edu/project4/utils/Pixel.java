2c03c9a829aa210f0a03a0180c025e5ad05863c3
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-02 11:22:13 +0300

    change from record to class with mutable fields

--- src/main/java/edu/project4/utils/Pixel.java
+++ src/main/java/edu/project4/utils/Pixel.java
@@ -1,3 +1,28 @@
 package edu.project4.utils;
 
-public record Pixel(Color color, int hitCount) { }
+public class Pixel {
+    public Pixel(Color color) {
+        this.color = color;
+        hitCount = 0;
+    }
+
+    public int getHitCount() {
+        return hitCount;
+    }
+
+    public Pixel hit() {
+        ++hitCount;
+        return this;
+    }
+
+    public void setColor(Color toSet) {
+        this.color = Color.of(toSet);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    private Color color;
+    private int hitCount;
+}
