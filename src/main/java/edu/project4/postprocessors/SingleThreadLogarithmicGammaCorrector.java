5278c6826c024e2ea57beb23e9a40cbf5849b18a
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-02 11:35:54 +0300

    init

---
+++ src/main/java/edu/project4/postprocessors/SingleThreadLogarithmicGammaCorrector.java
@@ -0,0 +1,10 @@
+package edu.project4.postprocessors;
+
+import edu.project4.FractalImage;
+
+final public class SingleThreadLogarithmicGammaCorrector implements ImageProcessor {
+    @Override
+    public void process(FractalImage image) {
+
+    }
+}
