0a84f4a40186f527d2d2409374327b1bcaaffeb2
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-02 11:22:29 +0300

    new Pixel usage

--- src/main/java/edu/project4/utils/ImageUtils.java
+++ src/main/java/edu/project4/utils/ImageUtils.java
@@ -25,7 +25,7 @@ public final class ImageUtils {
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                Color curPixelColor = source.data()[y][x].color();
+                Color curPixelColor = source.data()[y][x].getColor();
                 int rgb = (curPixelColor.r() << 16) | (curPixelColor.g() << 8) | curPixelColor.b();
                 res.setRGB(x, (height - y) - 1, rgb);
             }
