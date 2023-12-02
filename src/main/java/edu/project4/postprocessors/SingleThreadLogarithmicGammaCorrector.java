bbcd2f9646de969a3d00b7afb20edfcbc58bec38
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-02 19:28:21 +0300

    done

--- src/main/java/edu/project4/postprocessors/SingleThreadLogarithmicGammaCorrector.java
+++ src/main/java/edu/project4/postprocessors/SingleThreadLogarithmicGammaCorrector.java
@@ -1,10 +1,46 @@
 package edu.project4.postprocessors;
 
 import edu.project4.FractalImage;
+import edu.project4.utils.Color;
 
 final public class SingleThreadLogarithmicGammaCorrector implements ImageProcessor {
     @Override
-    public void process(FractalImage image) {
+    public void process(FractalImage image, double gamma) {
+        double max = 0.0;
 
+        for (int row = 0; row < image.height(); row++) {
+            for (int col = 0; col < image.width(); col++) {
+                var currentPixel = image.pixel(col, row);
+                if (currentPixel.getHitCount() != 0) {
+                    currentPixel.normal = Math.log10(currentPixel.getHitCount());
+                    max = Math.max(max, currentPixel.normal);
+                }
+            }
+        }
+
+        for (int row = 0; row < image.height(); row++) {
+            for (int col = 0; col < image.width(); col++) {
+                var currentPixel = image.pixel(col, row);
+
+                if (currentPixel.getHitCount() == 0) {
+                    continue;
+                }
+
+                currentPixel.normal /= max;
+                double correctionCoefficient = Math.pow(currentPixel.normal, (1.0/gamma));
+
+                int newRed = (int) Math.round(
+                    currentPixel.getColor().r() * correctionCoefficient
+                );
+                int newGreen = (int) Math.round(
+                    currentPixel.getColor().g() * correctionCoefficient
+                );
+                int newBlue = (int) Math.round(
+                    currentPixel.getColor().b() * correctionCoefficient
+                );
+
+                currentPixel.setColor(Color.of(newRed, newGreen, newBlue));
+            }
+        }
     }
 }
