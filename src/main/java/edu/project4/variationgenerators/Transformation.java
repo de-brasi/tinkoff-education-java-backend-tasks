03158d2dac0aa742f5fa216f1cb4db306590f119
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-02 11:23:42 +0300

    add opportunity to set color

--- src/main/java/edu/project4/variationgenerators/Transformation.java
+++ src/main/java/edu/project4/variationgenerators/Transformation.java
@@ -1,7 +1,27 @@
 package edu.project4.variationgenerators;
 
+import edu.project4.utils.Color;
 import edu.project4.utils.Point;
 import java.util.function.Function;
 
 @FunctionalInterface
-public interface Transformation extends Function<Point, Point> {}
+public interface Transformation extends Function<Point, Point> {
+    default Transformation withColor(Color toSet) {
+        var sourceTransformation = this;
+        return new Transformation() {
+            @Override
+            public Point apply(Point point) {
+                return sourceTransformation.apply(point);
+            }
+
+            @Override
+            public Color getColor() {
+                return toSet;
+            }
+        };
+    }
+
+    default Color getColor() {
+        return Color.of(0, 0, 0);
+    }
+}
