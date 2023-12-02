7dacb7ef9f3b720214291813838e159507301424
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-02 20:11:03 +0300

    done

---
+++ src/main/java/edu/project4/utils/ColorShortcuts.java
@@ -0,0 +1,29 @@
+package edu.project4.utils;
+
+public class ColorShortcuts {
+    private ColorShortcuts() {}
+
+    /* See more: https://colorscheme.ru/html-colors.html */
+
+    public final static Color BLACK = Color.of(0, 0, 0);
+    public final static Color WHITE = Color.of(255, 255, 255);
+    public final static Color GRAY = Color.of(128, 128, 128);
+    public final static Color SILVER = Color.of(192, 192, 192);
+    public final static Color FUCHSIA = Color.of(255, 0, 255);
+    public final static Color PURPLE = Color.of(128, 0, 128);
+    public final static Color RED = Color.of(255, 0, 0);
+    public final static Color MAROON = Color.of(128, 0, 0);
+    public final static Color YELLOW = Color.of(255, 255, 0);
+    public final static Color LIME = Color.of(0, 255, 0);
+    public final static Color GREEN = Color.of(0, 128, 0);
+    public final static Color AQUA = Color.of(0, 255, 255);
+    public final static Color TEAL = Color.of(0, 128, 128);
+    public final static Color BLUE = Color.of(0, 0, 255);
+    public final static Color NAVY = Color.of(0, 0, 128);
+    public final static Color CRIMSON = Color.of(220, 20, 60);
+    public final static Color DARK_RED = Color.of(139, 0, 0);
+    public final static Color HOT_PINK = Color.of(255, 20, 147);
+    public final static Color MEDIUM_VIOLET_RED = Color.of(199, 21, 133);
+    public final static Color GOLD = Color.of(255, 215, 0);
+    public final static Color STEEL_BLUE = Color.of(70, 130, 180);
+}
