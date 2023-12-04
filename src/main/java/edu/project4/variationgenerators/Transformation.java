71fba9d7cd356066ee284521862de6e21526ec3d
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-04 10:08:30 +0300

    add field with type (LINEAR, NON_LINEAR, ANY) and set-type functions

--- src/main/java/edu/project4/variationgenerators/Transformation.java
+++ src/main/java/edu/project4/variationgenerators/Transformation.java
@@ -21,7 +21,65 @@ public interface Transformation extends Function<Point, Point> {
         };
     }
 
+    default Transformation linear() {
+        var sourceTransformation = this;
+        return new Transformation() {
+            @Override
+            public Point apply(Point point) {
+                return sourceTransformation.apply(point);
+            }
+
+            @Override
+            public Color getColor() {
+                return sourceTransformation.getColor();
+            }
+
+            @Override
+            public Transformation withColor(Color toSet) {
+                return sourceTransformation.withColor(toSet);
+            }
+
+            @Override
+            public Type getType() {
+                return Type.LINEAR;
+            }
+        };
+    }
+
+    default Transformation nonLinear() {
+        var sourceTransformation = this;
+        return new Transformation() {
+            @Override
+            public Point apply(Point point) {
+                return sourceTransformation.apply(point);
+            }
+
+            @Override
+            public Color getColor() {
+                return sourceTransformation.getColor();
+            }
+
+            @Override
+            public Transformation withColor(Color toSet) {
+                return sourceTransformation.withColor(toSet);
+            }
+
+            @Override
+            public Type getType() {
+                return Type.NON_LINEAR;
+            }
+        };
+    }
+
     default Color getColor() {
         return Color.of(0, 0, 0);
+    }
+
+    default Transformation.Type getType() {
+        return Type.ANY;
+    }
+
+    enum Type {
+        LINEAR, NON_LINEAR, ANY
     }
 }
