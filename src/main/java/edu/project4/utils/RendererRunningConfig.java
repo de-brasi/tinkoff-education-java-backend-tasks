4e4c9145d09f61b706f871708de300ceba420030
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-04 10:43:22 +0300

    add samples count to arguments

--- src/main/java/edu/project4/utils/RendererRunningConfig.java
+++ src/main/java/edu/project4/utils/RendererRunningConfig.java
@@ -3,6 +3,7 @@ package edu.project4.utils;
 import edu.project4.variationgenerators.Transformation;
 
 public record RendererRunningConfig(
+    int samplesCount,
     short missedIterationsCount, Transformation.Type missedIterationsType,
     short mainIterationsCount, Transformation.Type mainIterationsType,
     int symmetry
