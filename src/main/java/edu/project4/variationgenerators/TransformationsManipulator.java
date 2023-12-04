46815bd9e79332e92dc97c549589d7bf28e1abbb
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-04 11:13:33 +0300

    (almost) correct functions for getting specified transformations

--- src/main/java/edu/project4/variationgenerators/TransformationsManipulator.java
+++ src/main/java/edu/project4/variationgenerators/TransformationsManipulator.java
@@ -1,11 +1,23 @@
 package edu.project4.variationgenerators;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class TransformationsManipulator {
     public TransformationsManipulator(List<Transformation> variations) {
-        transformations = variations;
+        transformations = new ArrayList<>(variations);
+
+        linearTransformations = new ArrayList<>();
+        nonLinearTransformations = new ArrayList<>();
+
+        for (var variation: variations) {
+            if (variation.getType() == Transformation.Type.LINEAR) {
+                linearTransformations.add(variation);
+            } else if (variation.getType() == Transformation.Type.NON_LINEAR) {
+                nonLinearTransformations.add(variation);
+            }
+        }
     }
 
     public Transformation getRandom() {
@@ -22,15 +34,27 @@ public class TransformationsManipulator {
     }
 
     public Transformation getRandomLinear() {
-        // TODO
-        return transformations.get(random.nextInt(0, transformations.size()));
+        // TODO: проверять что-то есть, иначе возвращать идемпотентную трансформацию
+        return linearTransformations.get(
+            random.nextInt(
+                0,
+                linearTransformations.size()
+            )
+        );
     }
 
     public Transformation getRandomNonLinear() {
-        // TODO
-        return transformations.get(random.nextInt(0, transformations.size()));
+        // TODO: проверять что-то есть, иначе возвращать идемпотентную трансформацию
+        return nonLinearTransformations.get(
+            random.nextInt(
+                0,
+                nonLinearTransformations.size()
+            )
+        );
     }
 
-    private final List<Transformation> transformations;
+    private final ArrayList<Transformation> transformations;
+    private final ArrayList<Transformation> linearTransformations;
+    private final ArrayList<Transformation> nonLinearTransformations;
     private final Random random = new Random();
 }
