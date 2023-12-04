690e45db8c287d0f3f3354d750813760a0833068
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-04 10:49:49 +0300

    add method for getting transformation according type

--- src/main/java/edu/project4/variationgenerators/TransformationsManipulator.java
+++ src/main/java/edu/project4/variationgenerators/TransformationsManipulator.java
@@ -12,6 +12,15 @@ public class TransformationsManipulator {
         return transformations.get(random.nextInt(0, transformations.size()));
     }
 
+    public Transformation getRandom(Transformation.Type type) {
+        return switch (type) {
+            case ANY -> getRandom();
+            case LINEAR -> getRandomLinear();
+            case NON_LINEAR -> getRandomNonLinear();
+            case null, default -> throw new RuntimeException("Unexpected argument!");
+        };
+    }
+
     public Transformation getRandomLinear() {
         // TODO
         return transformations.get(random.nextInt(0, transformations.size()));
