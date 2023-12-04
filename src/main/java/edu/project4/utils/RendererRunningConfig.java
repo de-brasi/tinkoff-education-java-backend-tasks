93ae930221775bbe2709462c88f0d6a8ff0aad82
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-04 10:38:14 +0300

    upd iterationsCount data type

--- src/main/java/edu/project4/utils/RendererRunningConfig.java
+++ src/main/java/edu/project4/utils/RendererRunningConfig.java
@@ -3,7 +3,7 @@ package edu.project4.utils;
 import edu.project4.variationgenerators.Transformation;
 
 public record RendererRunningConfig(
-    int missedIterationsCount, Transformation.Type missedIterationsType,
-    int mainIterationsCount, Transformation.Type mainIterationsType,
+    short missedIterationsCount, Transformation.Type missedIterationsType,
+    short mainIterationsCount, Transformation.Type mainIterationsType,
     int symmetry
 ) {}
