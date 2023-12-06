5c9586d109b1aa43320602106e65b63832b97a7e
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-06 14:30:04 +0300

    add method for generating Point from current point with applying coefficients

--- src/main/java/edu/project4/utils/Point.java
+++ src/main/java/edu/project4/utils/Point.java
@@ -4,4 +4,11 @@ public record Point(double x, double y) {
     public static Point of(double x, double y) {
         return new Point(x, y);
     }
+
+    public Point withCoefficients(double a, double b, double c, double d, double e, double f) {
+        return new Point(
+            x * a + y * b + c,
+            x * d + y * e + f
+        );
+    }
 }
