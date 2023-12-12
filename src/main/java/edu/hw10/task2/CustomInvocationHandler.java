cd916e62fc2a28621901bc36db8c9677a20f1b32
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-12 16:09:43 +0300

    add constructor with Path to tmp file; add support of saving cached data to JSON file

--- src/main/java/edu/hw10/task2/CustomInvocationHandler.java
+++ src/main/java/edu/hw10/task2/CustomInvocationHandler.java
@@ -2,6 +2,10 @@ package edu.hw10.task2;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,6 +14,12 @@ public class CustomInvocationHandler implements InvocationHandler {
 
     public CustomInvocationHandler(Object target) {
         this.target = target;
+        this.filePathForSave = DEFAULT_PATH;
+    }
+
+    public CustomInvocationHandler(Object target, Path pathFileForSave) {
+        this.target = target;
+        this.filePathForSave = pathFileForSave;
     }
 
     @Override
@@ -28,7 +38,7 @@ public class CustomInvocationHandler implements InvocationHandler {
             result = method.invoke(target, args);
             methodsCallCache.put(newCallInfo, result);
 
-            // TODO: если надо кэшировать, то записать в JSON файл
+            saveData();
         }
 
         System.out.println("After invoking independent custom method: " + method.getName());
@@ -36,7 +46,61 @@ public class CustomInvocationHandler implements InvocationHandler {
         return result;
     }
 
+    private void saveData() {
+        try {
+            if (Files.notExists(this.filePathForSave)) {
+                Files.createFile(this.filePathForSave);
+            }
+
+            String json = convertCacheToJson();
+            Files.writeString(this.filePathForSave, json, StandardOpenOption.TRUNCATE_EXISTING);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String convertCacheToJson() {
+        StringBuilder jsonBuilder = new StringBuilder("[\n");
+
+        methodsCallCache.forEach((methodCallInfo, value) -> {
+            String method = methodCallInfo.method().getName();
+            Object[] args = methodCallInfo.args();
+            String argsJson = convertArgsToJson(args);
+
+            jsonBuilder.append(String.format("  {\"method\": \"%s\", \"args\": %s},\n", method, argsJson));
+        });
+
+        if (!methodsCallCache.isEmpty()) {
+            jsonBuilder.delete(jsonBuilder.length() - 2, jsonBuilder.length());
+        }
+
+        jsonBuilder.append("]\n");
+        return jsonBuilder.toString();
+    }
+
+    private static String convertArgsToJson(Object[] args) {
+        if (args == null) {
+            return "[]";
+        }
+
+        StringBuilder argsJsonBuilder = new StringBuilder("[");
+
+        for (Object arg : args) {
+            argsJsonBuilder.append(String.format("\"%s\",", arg.toString()));
+        }
+
+        if (args.length > 0) {
+            argsJsonBuilder.delete(argsJsonBuilder.length() - 1, argsJsonBuilder.length());
+        }
+
+        argsJsonBuilder.append("]");
+        return argsJsonBuilder.toString();
+    }
+
     private final Map<MethodCallInfo, Object> methodsCallCache = new HashMap<>();
+    private final Path filePathForSave;
+    private final Path DEFAULT_PATH = Paths.get(System.getProperty("user.home"), "saving.json");
 
     private static record MethodCallInfo(Method method, Object[] args) {}
 }
