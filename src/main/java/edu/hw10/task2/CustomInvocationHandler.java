917849dd3945ab14584fe6b8ab1b68d8b7a1e964
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-12 14:15:34 +0300

    better

--- src/main/java/edu/hw10/task2/CustomInvocationHandler.java
+++ src/main/java/edu/hw10/task2/CustomInvocationHandler.java
@@ -15,11 +15,13 @@ public class CustomInvocationHandler implements InvocationHandler {
         if (method.isAnnotationPresent(Cache.class) && method.getAnnotation(Cache.class).persist()) {
             System.out.println("Cached method: " + method.getName());
         }
+
         System.out.println("Before invoking independent custom method: " + method.getName());
-        // You can perform additional tasks here.
+
         Object result = method.invoke(target, args); // Delegate to the real object.
-        // You can perform additional tasks here.
+
         System.out.println("After invoking independent custom method: " + method.getName());
+
         return result;
     }
 }
