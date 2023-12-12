5216b8e3a50e00eea4f33d6e9da57cc07317bb91
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-12 16:20:46 +0300

    add caching annotation

--- src/test/java/edu/hw10/testinginterfaces/LongProcessOperationInterface.java
+++ src/test/java/edu/hw10/testinginterfaces/LongProcessOperationInterface.java
@@ -1,5 +1,8 @@
 package edu.hw10.testinginterfaces;
 
+import edu.hw10.task2.Cache;
+
 public interface LongProcessOperationInterface {
+    @Cache(persist = true)
     int longProcess() throws InterruptedException;
 }
