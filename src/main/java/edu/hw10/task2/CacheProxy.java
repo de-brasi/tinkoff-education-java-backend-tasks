cefde1bf4adce56821951861b97d2ed020912526
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-12 16:09:01 +0300

    add constructor with Path to tmp file

--- src/main/java/edu/hw10/task2/CacheProxy.java
+++ src/main/java/edu/hw10/task2/CacheProxy.java
@@ -1,6 +1,7 @@
 package edu.hw10.task2;
 
 import java.lang.reflect.Proxy;
+import java.nio.file.Path;
 
 public class CacheProxy {
     private CacheProxy() {}
@@ -10,6 +11,14 @@ public class CacheProxy {
             CacheProxy.class.getClassLoader(),
             new Class<?>[] {interfaceToProxy},
             new CustomInvocationHandler(obj)
+        );
+    }
+
+    public static Object create(Object obj, Class<?> interfaceToProxy, Path pathFileForSave) {
+        return Proxy.newProxyInstance(
+            CacheProxy.class.getClassLoader(),
+            new Class<?>[] {interfaceToProxy},
+            new CustomInvocationHandler(obj, pathFileForSave)
         );
     }
 }
