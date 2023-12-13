fa9451c7facb83214ff3b978a0f326ddb24af00d
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-13 14:49:36 +0300

    task 1 (with test)

---
+++ src/test/java/edu/hw11/Task1Test.java
@@ -0,0 +1,23 @@
+package edu.hw11;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.FixedValue;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.InvocationTargetException;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task1Test {
+    @Test
+    public void test()
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<?> exampleType = new ByteBuddy()
+            .subclass(Object.class)
+            .method(named("toString")).intercept(FixedValue.value("Hello World!"))
+            .make()
+            .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+            .getLoaded();
+        assertThat(exampleType.getDeclaredConstructor().newInstance().toString()).isEqualTo("Hello World!");
+    }
+}
