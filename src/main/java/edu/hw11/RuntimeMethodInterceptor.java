0e80b972f1d40fdbba9f6496a328181747785032
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-14 18:20:27 +0300

    init and done (how to choose donor method name?)

---
+++ src/main/java/edu/hw11/RuntimeMethodInterceptor.java
@@ -0,0 +1,28 @@
+package edu.hw11;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.implementation.bind.annotation.TargetMethodAnnotationDrivenBinder;
+
+import java.util.Arrays;
+import static net.bytebuddy.implementation.MethodDelegation.to;
+import static net.bytebuddy.implementation.MethodDelegation.withDefaultConfiguration;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public class RuntimeMethodInterceptor {
+    private RuntimeMethodInterceptor() {}
+
+    public static void interceptMethod(Class<?> target, Class<?> donor, String methodName) {
+        ByteBuddyAgent.install();
+        new ByteBuddy()
+            .redefine(target)
+            .method(named(methodName))
+            .intercept(to(donor))
+            .make()
+            .load(
+                target.getClassLoader(),
+                ClassReloadingStrategy.fromInstalledAgent()
+            );
+    }
+}
