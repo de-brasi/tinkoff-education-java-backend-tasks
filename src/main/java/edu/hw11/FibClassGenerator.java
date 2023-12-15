d4286f4324dfb4a6f77529d681c94c41e7b84055
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-15 19:54:08 +0300

    add todo

--- src/main/java/edu/hw11/FibClassGenerator.java
+++ src/main/java/edu/hw11/FibClassGenerator.java
@@ -3,17 +3,13 @@ package edu.hw11;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.dynamic.scaffold.InstrumentedType;
-import net.bytebuddy.implementation.FixedValue;
 import net.bytebuddy.implementation.Implementation;
 import net.bytebuddy.implementation.bytecode.ByteCodeAppender;
 import net.bytebuddy.implementation.bytecode.StackManipulation;
 import net.bytebuddy.implementation.bytecode.constant.TextConstant;
 import net.bytebuddy.implementation.bytecode.member.MethodReturn;
 import net.bytebuddy.jar.asm.MethodVisitor;
-import net.bytebuddy.matcher.ElementMatchers;
 import org.jetbrains.annotations.NotNull;
-import org.objectweb.asm.Opcodes;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.returns;
 import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
@@ -25,7 +21,7 @@ public class FibClassGenerator {
         // TODO:
         //  1) сделать класс с bytebuddy;                                                          - done
         //  2) сделать метод у класса с помощью bytebuddy (простой метод который вернет строку);   - done
-        //  3) метод при помощи байткода bytebuddy
+        //  3) метод при помощи байткода (методы из bytebuddy)                                     - done
         //  4) сделать этот метод на ASM;
         //  5) метод fib на ASM
 
