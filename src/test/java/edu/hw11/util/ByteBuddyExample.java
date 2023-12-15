6edf154517546779f2bdc1817d33116f74de2ae8
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-15 23:48:05 +0300

    finally done FibComputingAppender

--- src/test/java/edu/hw11/util/ByteBuddyExample.java
+++ src/test/java/edu/hw11/util/ByteBuddyExample.java
@@ -4,11 +4,13 @@ import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.modifier.Visibility;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.Implementation;
 import net.bytebuddy.implementation.bytecode.ByteCodeAppender;
 import net.bytebuddy.implementation.bytecode.StackManipulation;
 import net.bytebuddy.implementation.bytecode.constant.TextConstant;
 import net.bytebuddy.implementation.bytecode.member.MethodReturn;
+import net.bytebuddy.jar.asm.Label;
 import net.bytebuddy.jar.asm.MethodVisitor;
 import net.bytebuddy.jar.asm.Opcodes;
 import org.jetbrains.annotations.NotNull;
@@ -27,15 +29,26 @@ public class ByteBuddyExample {
             .defineMethod("customIncr", int.class, Visibility.PUBLIC)
             .withParameters(int.class)
             .intercept(new Implementation.Simple(new IncrementVarAppender()))
+            .defineMethod("fib", int.class, Visibility.PUBLIC)
+            .withParameters(int.class)
+            .intercept(new Implementation.Simple(new FibComputingAppender()))
             .make();
 
-        Class<?> dynamicClass = dynamicType.load(ByteBuddyExample.class.getClassLoader())
+        Class<?> dynamicClass = dynamicType.load(
+            ByteBuddyExample.class.getClassLoader(),
+                ClassLoadingStrategy.Default.WRAPPER)
             .getLoaded();
 
         Object instance = dynamicClass.newInstance();
 
         System.out.println(instance.getClass().getMethod("hello").invoke(instance));
         System.out.println(instance.getClass().getMethod("customIncr", int.class).invoke(instance, 3));
+
+        System.out.println();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(instance.getClass().getMethod("fib", int.class).invoke(instance, i));
+        }
     }
 
     public static class CustomByteCodeAppender implements ByteCodeAppender {
@@ -69,6 +82,65 @@ public class ByteBuddyExample {
             methodVisitor.visitEnd();
 
             return new Size(2, 1);
+        }
+    }
+
+    public static class FibComputingAppender implements ByteCodeAppender {
+        @Override
+        public @NotNull Size apply(
+            @NotNull MethodVisitor methodVisitor,
+            Implementation.@NotNull Context context,
+            @NotNull MethodDescription methodDescription
+        ) {
+            var label1 = new Label();
+            var label2 = new Label();
+
+            methodVisitor.visitCode();
+
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
+            methodVisitor.visitInsn(Opcodes.ICONST_1);
+
+            methodVisitor.visitJumpInsn(Opcodes.IF_ICMPNE, label1);
+            methodVisitor.visitInsn(Opcodes.ICONST_0);
+            methodVisitor.visitInsn(Opcodes.IRETURN);
+
+            methodVisitor.visitLabel(label1);
+            methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
+            methodVisitor.visitInsn(Opcodes.ICONST_2);
+
+            methodVisitor.visitJumpInsn(Opcodes.IF_ICMPNE, label2);
+            methodVisitor.visitInsn(Opcodes.ICONST_1);
+            methodVisitor.visitInsn(Opcodes.IRETURN);
+
+            methodVisitor.visitLabel(label2);
+            methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
+            methodVisitor.visitInsn(Opcodes.ICONST_1);
+            methodVisitor.visitInsn(Opcodes.ISUB);
+            methodVisitor.visitMethodInsn(
+                Opcodes.INVOKESTATIC,
+                "edu/hw11/util/FibExample",
+                "fib",
+                "(I)I"
+            );
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
+            methodVisitor.visitInsn(Opcodes.ICONST_2);
+            methodVisitor.visitInsn(Opcodes.ISUB);
+            methodVisitor.visitMethodInsn(
+                Opcodes.INVOKESTATIC,
+                "edu/hw11/util/FibExample",
+                "fib",
+                "(I)I"
+            );
+            methodVisitor.visitInsn(Opcodes.IADD);
+            methodVisitor.visitInsn(Opcodes.IRETURN);
+
+            methodVisitor.visitMaxs(3, 1);
+            methodVisitor.visitEnd();
+            return new Size(3, 1);
         }
     }
 }
