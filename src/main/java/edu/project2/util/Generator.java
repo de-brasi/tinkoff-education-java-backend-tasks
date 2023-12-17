6261dc92213ec83f4b87dd466ca6fd9b1d69b8f7
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-17 23:23:16 +0300

    delete extra code

--- src/main/java/edu/project2/util/Generator.java
+++
@@ -1,5 +0,0 @@
-package edu.project2.util;
-
-public interface Generator {
-    Maze generate(int width, int height);
-}
--- src/main/java/edu/project2/util/Solver.java
+++
@@ -1,7 +0,0 @@
-package edu.project2.util;
-
-import java.util.ArrayList;
-
-public interface Solver {
-    ArrayList<Coordinate> solve(Maze maze, Coordinate start, Coordinate end);
-}
