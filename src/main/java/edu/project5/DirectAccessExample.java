82393b6c8a8daf9796905df937f7ca644700e700
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-17 15:42:03 +0300

    logging accessed fields

--- src/main/java/edu/project5/DirectAccessExample.java
+++ src/main/java/edu/project5/DirectAccessExample.java
@@ -1,4 +1,17 @@
 package edu.project5;
 
+import edu.project5.targets.Student;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class DirectAccessExample {
+    private DirectAccessExample() {}
+
+    public static void main(String[] args) {
+        LOGGER.info(target.name());
+        LOGGER.info(target.surname());
+    }
+
+    private final static Student target = new Student("Some", "Student");
+    private final static Logger LOGGER = LogManager.getLogger();
 }
