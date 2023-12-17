7a6de8a0522f954992b8b91dbb6173aba2ffa86b
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-17 15:50:26 +0300

    logging accessed fields

--- src/main/java/edu/project5/ReflectMethodAccessExample.java
+++ src/main/java/edu/project5/ReflectMethodAccessExample.java
@@ -1,4 +1,23 @@
 package edu.project5;
 
+import edu.project5.targets.Student;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.lang.reflect.InvocationTargetException;
+
 public class ReflectMethodAccessExample {
+    private ReflectMethodAccessExample() {}
+
+    public static void main(String[] args)
+        throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        LOGGER.info(
+            target.getClass().getMethod("name").invoke(target)
+        );
+        LOGGER.info(
+            target.getClass().getMethod("surname").invoke(target)
+        );
+    }
+
+    private final static Student target = new Student("Some", "Student");
+    private final static Logger LOGGER = LogManager.getLogger();
 }
