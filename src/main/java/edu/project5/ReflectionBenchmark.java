862a11749a53a15ad3e5ea8f4be13552b9fef7f4
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-17 22:12:48 +0300

    prepared for tests

--- src/main/java/edu/project5/ReflectionBenchmark.java
+++ src/main/java/edu/project5/ReflectionBenchmark.java
@@ -1,5 +1,9 @@
 package edu.project5;
 
+import java.lang.invoke.LambdaMetafactory;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
@@ -29,23 +33,49 @@ public class ReflectionBenchmark {
             .warmupIterations(1)
             .warmupTime(TimeValue.seconds(5))
             .measurementIterations(1)
-            .measurementTime(TimeValue.seconds(5))
+            .measurementTime(TimeValue.seconds(180))
             .build();
 
         new Runner(options).run();
     }
 
-    record Student(String name, String surname) {
-    }
+    record Student(String name, String surname) {}
 
     private Student student;
     private Method method;
+    private MethodHandle methodHandle;
+    StudentNameInfoGetter nameGetter;
 
     @Setup
-    public void setup() throws NoSuchMethodException {
-        // TODO: подготовительные работы
+    public void setup() throws Throwable {
         student = new Student("SomeName", "SomeSurname");
-        method = ...;
+        method = student.getClass().getMethod("name");
+
+        // using java.lang.invoke.MethodHandles
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        MethodType methodType = MethodType.methodType(String.class);
+
+        methodHandle = lookup.findVirtual(Student.class, "name", methodType);
+
+        // using java.lang.invoke.LambdaMetafactory
+        MethodHandles.Lookup caller = MethodHandles.lookup();
+        MethodType getterNameType = MethodType.methodType(StudentNameInfoGetter.class);
+        MethodType methodGetterType = MethodType.methodType(String.class, Student.class);
+        MethodHandle originalCalledGenNameMethod = caller.findVirtual(
+            Student.class, "name", MethodType.methodType(String.class)
+        );
+        MethodType instantiatedMethodType = MethodType.methodType(String.class, Student.class);
+
+        nameGetter = (StudentNameInfoGetter) LambdaMetafactory.metafactory(
+                caller,
+                "get",
+                getterNameType,
+                methodGetterType,
+                originalCalledGenNameMethod,
+                instantiatedMethodType
+            )
+            .getTarget()
+            .invokeExact();
     }
 
     @Benchmark
@@ -54,21 +84,29 @@ public class ReflectionBenchmark {
         bh.consume(name);
     }
 
+
     @Benchmark
     public void reflection(Blackhole bh) throws InvocationTargetException, IllegalAccessException {
-        // TODO
+        Object name = method.invoke(student);
         bh.consume(name);
     }
 
+
     @Benchmark
-    public void invokeMethodHandlers(Blackhole bh) throws InvocationTargetException, IllegalAccessException {
-        // TODO
+    public void invokeMethodHandlers(Blackhole bh) throws Throwable {
+        var name = methodHandle.invoke(student);
         bh.consume(name);
     }
 
+
     @Benchmark
-    public void invokeLambdaMetafactory(Blackhole bh) throws InvocationTargetException, IllegalAccessException {
-        // TODO
+    public void invokeLambdaMetafactory(Blackhole bh) {
+        var name = nameGetter.get(student);
         bh.consume(name);
+    }
+
+
+    private interface StudentNameInfoGetter {
+        String get(Student target);
     }
 }
