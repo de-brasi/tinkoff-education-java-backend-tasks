f39ae79e1db5893cca3d73b24df90f5a18735275
de-brasi <leskin.ilia.public@yandex.ru>
2023-12-18 22:50:05 +0300

    add test like in task description, fix another tests according changed public methods signature

--- src/test/java/edu/hw2/Task2Test.java
+++ src/test/java/edu/hw2/Task2Test.java
@@ -4,6 +4,9 @@ import edu.hw2.task2.Rectangle;
 import edu.hw2.task2.Square;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task2Test {
@@ -24,8 +27,28 @@ public class Task2Test {
     @Test
     @DisplayName("Changing side value")
     void test3() {
-        Rectangle rectangleLike = new Square(10);
-        var changedSide = rectangleLike.setHeight(2);
-        assertThat(changedSide.area()).isEqualTo(4);
+        Rectangle square = new Square(10);
+        square.setHeight(2);
+        assertThat(square.area()).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @MethodSource("rectangles")
+    void rectangleArea(Rectangle rect) {
+        rect.setWidth(20);
+        rect.setHeight(10);
+
+        if (rect instanceof Square) {
+            assertThat(rect.area()).isEqualTo(100.0);
+        } else {
+            assertThat(rect.area()).isEqualTo(200.0);
+        }
+    }
+
+    private static Arguments[] rectangles() {
+        return new Arguments[]{
+            Arguments.of(new Rectangle()),
+            Arguments.of(new Square())
+        };
     }
 }
